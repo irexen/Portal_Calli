34d92fd1466386510cc3f89a7267450cbe980957
AlejandroRebollo <irexen77@yahoo.com>
2021-07-04 14:12:10 -0500

    Add new version

--- src/App.js
+++ src/App.js
@@ -12,15 +12,19 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 //Función principal de la aplicación
 function App() {
+  var Img = <img src="public\HOOD 2.png"></img>
+
   return (
   <div>
-    <Navbar  bg="dark" variant="dark">>
-    <Navbar.Toggle />
-    <Navbar.Collapse>
+    <Navbar bg="transparent" variant="transparent" expand="lg" sticky="top">
+    <Navbar.Brand variant="outline-info">
+    <img width="70px" height="auto" className="img-responsive" src="\HOOD 2.png"  alt="logo" />
+    <a class="navbar-brand">&nbsp;&nbsp;Portal Calli</a>
+    </Navbar.Brand>
     <Nav className="justify-content-end" style={{ width: "100%" }}>
-    <Button variant="outline-success">Inicio</Button>
+    <Button variant="outline-info">Inicio</Button>
     <ButtonGroup>
-    <DropdownButton variant="outline-success" title="Bancos">
+    <DropdownButton variant="outline-info" title="Bancos">
       <Dropdown.Item >Informe de Cuentas</Dropdown.Item>
       <Dropdown.Item >Conciliación de Cuentas</Dropdown.Item>
       <Dropdown.Item >Administración de Cuentas</Dropdown.Item>
@@ -30,7 +34,7 @@ function App() {
       <Dropdown.Item >Nueva Cuenta Extraordinaria</Dropdown.Item>
       <Dropdown.Item >Ajuste Cuotas de Mantenimiento</Dropdown.Item>              
     </DropdownButton>
-    <DropdownButton variant="outline-success" title="Administración">
+    <DropdownButton variant="outline-info" title="Administración">
       <Dropdown.Item >Administración de espacios</Dropdown.Item>
       <Dropdown.Item >Administración de usuarios</Dropdown.Item>
       <Dropdown.Item >Administración de Personal</Dropdown.Item>
@@ -38,12 +42,11 @@ function App() {
       <Dropdown.Item >Administración de bienes privados</Dropdown.Item>
       <Dropdown.Item >Administración de activos</Dropdown.Item>
     </DropdownButton>
-    <Button variant="outline-success">Proyectos</Button>
-    <Button variant="outline-success">Comunicación</Button>
-    <Button variant="outline-success">Gobierno</Button>
+    <Button variant="outline-info">Proyectos</Button>
+    <Button variant="outline-info">Comunicación</Button>
+    <Button variant="outline-info">Gobierno</Button>
     </ButtonGroup>
     </Nav>
-    </Navbar.Collapse>
     </Navbar>
     <br></br>
     <br></br>
