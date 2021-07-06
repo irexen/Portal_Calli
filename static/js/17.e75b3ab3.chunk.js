27df60e37c3db570569c415c23b5115d6adbb3ba
AlejandroRebollo <irexen77@yahoo.com>
2021-07-06 17:25:42 -0500

    Updates

---
+++ static/js/17.e75b3ab3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcalli-app"]=this["webpackJsonpcalli-app"]||[]).push([[17],{409:function(t,n,e){"use strict";e.r(n),e.d(n,"amplify_auth_container",(function(){return p}));var i=e(16),p=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("form",{autoComplete:"on",hidden:!0},Object(i.i)("input",{name:"username"}),Object(i.i)("input",{name:"password",type:"password"}),Object(i.i)("input",{type:"submit"})),Object(i.i)("slot",null))},t}()}}]);
+//# sourceMappingURL=17.e75b3ab3.chunk.js.map
